463f2d9f27142fef8f87b0f7556af39419961de8
cPen <1533431376@qq.com>
2022-07-10 23:05:31 +0800

    modify

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-echo -e '# image_bed Start'
+echo -e '# image_bed `git pull` Start'
 
 # 远端图床仓库（github）同步至本地
 git pull
 
-echo -e '# image_bed End\n'
+echo -e '# image_bed `git pull` End\n'
